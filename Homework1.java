30a6253a670d9d241fc861678c72d4e45820b20f
rlagns1234 <tonyk0521@naver.com>
2023-09-14 13:09:23 +0900

    1주차과제

--- Homework1.java
+++ Homework1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Subject1 {
+public class Homework1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 0;
